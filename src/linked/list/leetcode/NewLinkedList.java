2c3b1250c94aec3dd924ed49ed49c237fcb7b6ae
Steampunk <dasies11@gmail.com>
2021-09-12 22:31:39 +0200

    Add new algorithms to remove elements

--- src/linked/list/leetcode/NewLinkedList.java
+++ src/linked/list/leetcode/NewLinkedList.java
@@ -30,8 +30,9 @@ public class NewLinkedList {
         // Set new node to the final of the list
         current.next = newNode;
         // Increase list size
-        this.size ++;
+        this.size++;
     }
+
     protected boolean insertElement(int position, int val) {
         // Insert in tail
         int lastPosition = this.size - 1;
@@ -48,14 +49,14 @@ public class NewLinkedList {
                 // Iterate the list to the indicated position
                 Node previous = new Node();
                 int index = 0;
-                while (index ++ < position) {
+                while (index++ < position) {
                     previous = current;
                     current = current.next;
                 }
                 newNode.next = current;
                 previous.next = newNode;
             }
-            this.size ++;
+            this.size++;
             return true;
         }
         return false;
@@ -69,13 +70,13 @@ public class NewLinkedList {
         } else {
             Node previous = new Node();
             int index = 0;
-            while (index ++ < position) {
+            while (index++ < position) {
                 previous = current;
                 current = current.next;
             }
             previous.next = current.next;
         }
-        this.size --;
+        this.size--;
     }
 
     protected void removeByValue(int value) {
@@ -86,12 +87,12 @@ public class NewLinkedList {
     protected int indexOf(int value) {
         Node current = this.head;
         int position = 0;
-        while (current.next != null) {
-            current = current.next;
-            position ++;
+        while (current != null) {
             if (current.data == value) {
                 return position;
             }
+            position++;
+            current = current.next;
         }
         return -1;
     }
@@ -104,25 +105,113 @@ public class NewLinkedList {
         return this.size;
     }
 
+    // 83. Remove Duplicates from Sorted List
+    // Given the head of a sorted linked list, delete all duplicates such that each element appears only once.
+    // Return the linked list sorted as well.
+    protected Node removeDuplicates(Node head) {
+        int position = 0;
+        while (position != -1 && head != null) {
+            position = nodeIndexOf(head, head.data);
+            head = removeByNodeAndPosition(head, position);
+        }
+        return head;
+    }
+
+    // 203. Remove Linked List Elements
+    // Given the head of a linked list and an integer val,
+    // remove all the nodes of the linked list that has Node.val == val, and return the new head
+    protected Node removeByNodeAndValue(Node head, int value) {
+        int position = 0;
+        while (position != -1 && head != null) {
+            position = nodeIndexOf(head, value);
+            head = removeByNodeAndPosition(head, position);
+        }
+        return head;
+    }
+
+    protected Node removeByNodeAndPosition(Node head, int position) {
+        if (position >= 0) {
+            Node current = head;
+            // Remove head
+            if (position == 0) {
+                head = current.next;
+            } else {
+                Node previous = new Node();
+                int index = 0;
+                while (index++ < position) {
+                    previous = current;
+                    current = current.next;
+                }
+                previous.next = current.next;
+            }
+            this.size--;
+        }
+        return head;
+    }
+
+    protected int nodeIndexOf(Node head, int value) {
+        Node current = head;
+        int position = 0;
+        while (current != null) {
+            if (current.data == value) {
+                return position;
+            }
+            position++;
+            current = current.next;
+        }
+        return -1;
+    }
 
     // 206. Reverse Linked List
     // Given the head of a singly linked list, reverse the list, and return the reversed list.
     protected Node reverseList(Node head) {
-        Node current = head;
-        Stack<Integer> stack = new Stack<>();
-        while (current.next != null) {
+        if (head != null) {
+            Node current = head;
+            Stack<Integer> stack = new Stack<>();
+            while (current.next != null) {
+                stack.push(current.data);
+                current = current.next;
+            }
             stack.push(current.data);
-            current = current.next;
-        }
-        stack.push(current.data);
-        Node currentReverse = head;
-        while (currentReverse.next != null) {
+            Node currentReverse = head;
+            while (currentReverse.next != null) {
+                currentReverse.data = stack.pop();
+                currentReverse = currentReverse.next;
+            }
             currentReverse.data = stack.pop();
-            currentReverse = currentReverse.next;
         }
-        currentReverse.data = stack.pop();
         return head;
     }
 
+    // Iterative --> Review, it's too much complex solution
+    protected Node iterativeReverseList(Node head) {
+        if (head == null) return null;
+        Node curr = head;
+        Node temp = null;
+        Node next = curr.next;
+        while (curr != null) {
+            // Break first node
+            curr.next = temp;
+            temp = curr;
+            curr = next;
+            next = (curr != null) ? curr.next : null;
+        }
+        return temp;
+    }
+
+    // Recursive --> Review, it's too much complex solution
+    public Node recursiveReverseList(Node head) {
+        if (head == null) return null;
+        return solve(head, null, head.next);
+    }
+
+    private Node solve(Node curr, Node temp, Node next) {
+        if (curr == null) return temp;
+        curr.next = temp;
+        temp = curr;
+        curr = next;
+        next = (curr != null) ? curr.next : null;
+        return solve(curr, temp, next);
+    }
 
 }
--- src/linked/list/leetcode/NewLinkedListTest.java
+++ src/linked/list/leetcode/NewLinkedListTest.java
@@ -2,8 +2,7 @@ package linked.list.leetcode;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class NewLinkedListTest {
 
@@ -81,6 +80,54 @@ class NewLinkedListTest {
         assertEquals(linkedList.indexOf(4), 2);
     }
 
+    // 203. Remove Linked List Elements
+    @Test
+    void deleteNodes() {
+        // Given
+        NewLinkedList linkedList = new NewLinkedList();
+        // When
+        linkedList.appendElement(1);
+        linkedList.appendElement(2);
+        linkedList.appendElement(6);
+        linkedList.appendElement(3);
+        linkedList.appendElement(4);
+        linkedList.appendElement(5);
+        linkedList.appendElement(6);
+        NewLinkedList.Node head = linkedList.head;
+        NewLinkedList.Node result = linkedList.removeByNodeAndValue(head, 6);
+        // Then
+        assertEquals(linkedList.size, 5);
+        assertEquals(result.data, 1);
+    }
+
+    @Test
+    void deleteNodes1() {
+        // Given
+        NewLinkedList linkedList = new NewLinkedList();
+        // When
+        NewLinkedList.Node head = linkedList.head;
+        NewLinkedList.Node result = linkedList.removeByNodeAndValue(head, 1);
+        // Then
+        assertEquals(linkedList.size, 0);
+        assertNull(result);
+    }
+
+    @Test
+    void deleteNodes2() {
+        // Given
+        NewLinkedList linkedList = new NewLinkedList();
+        // When
+        linkedList.appendElement(7);
+        linkedList.appendElement(7);
+        linkedList.appendElement(7);
+        linkedList.appendElement(7);
+        NewLinkedList.Node head = linkedList.head;
+        NewLinkedList.Node result = linkedList.removeByNodeAndValue(head, 7);
+        // Then
+        assertEquals(linkedList.size, 0);
+        assertNull(result);
+    }
+
     @Test
     void reverseLinkedList() {
         // Given
@@ -93,7 +140,43 @@ class NewLinkedListTest {
         linkedList.appendElement(5);
         // Then
         linkedList.reverseList(linkedList.head);
-        assertEquals(linkedList.size, 5);
+        assertEquals(linkedList.head.data, 5);
+    }
+
+    @Test
+    void reverseEmptyLinkedList() {
+        // Given
+        NewLinkedList linkedList = new NewLinkedList();
+        // When
+        // Then
+        linkedList.reverseList(linkedList.head);
+        assertEquals(linkedList.size, 0);
+        assertNull(linkedList.head);
+    }
+    @Test
+    void iterativeReverseLinkedList() {
+        // Given
+        NewLinkedList linkedList = new NewLinkedList();
+        // When
+        linkedList.appendElement(1);
+        linkedList.appendElement(2);
+        linkedList.appendElement(3);
+        // Then
+        NewLinkedList.Node result = linkedList.iterativeReverseList(linkedList.head);
+        assertEquals(result.data, 3);
+    }
+
+    @Test
+    void recursiveReverseLinkedList() {
+        // Given
+        NewLinkedList linkedList = new NewLinkedList();
+        // When
+        linkedList.appendElement(1);
+        linkedList.appendElement(2);
+        linkedList.appendElement(3);
+        // Then
+        NewLinkedList.Node result = linkedList.recursiveReverseList(linkedList.head);
+        assertEquals(result.data, 3);
     }
 
 }
