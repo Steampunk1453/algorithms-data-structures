422861c323adc65538e6e15f91b1aa71f742ece2
Steampunk <dasies11@gmail.com>
2023-01-12 11:43:48 +0100

    Add flatten method for double linked List. Comment because it needs to create child node

--- src/linked/list/top/DoublyLinkedList.java
+++ src/linked/list/top/DoublyLinkedList.java
@@ -1,7 +1,6 @@
 package linked.list.top;
 
 public class DoublyLinkedList {
-
     Node head;
     Node tail;
 
@@ -13,7 +12,6 @@ public class DoublyLinkedList {
         public Node(int data) {
             this.data = data;
         }
-
     }
 
     protected void insert(int value) {
@@ -39,5 +37,34 @@ public class DoublyLinkedList {
             current = current.previous;
         }
     }
+
+    public LinkedListExe.Node findTail(LinkedListExe.Node child){
+        while(child.next != null){
+            child=child.next;
+        }
+        return child;
+    }
+
+    // 430. Flatten a Multilevel Doubly Linked List
+    // Time complexity: O(n)
+    // Space complexity: O(1)
+/*    protected Node flatten(Node head) {
+        Node curr = head;
+        while(curr!=null){
+            if(curr.child != null){
+                Node tail = findTail(curr.child);
+                if(curr.next != null){
+                    curr.next.prev=tail;
+                }
+
+                tail.next = curr.next;
+                curr.next = curr.child;
+                curr.child.prev = curr;
+                curr.child = null;
+            }
+            curr = curr.next;
+        }
+        return head;
+    }*/
 
 }
