cfe2f1fc9b4de6281071d6d268501332ec13c57e
Steampunk <dasies11@gmail.com>
2023-02-21 14:43:11 +0100

    Add find first and last position in array

--- src/binary/search/BinarySearch.java
+++ src/binary/search/BinarySearch.java
@@ -1,6 +1,7 @@
 package binary.search;
 
 public class BinarySearch {
+
     protected int binarySearch(int[] array, int key) {
         if (isEmpty(array)) {
             return -1;
@@ -25,6 +26,38 @@ public class BinarySearch {
     private static boolean isEmpty(int[] array) {
         int arraySize = array.length;
         return arraySize == 0;
+    }
+
+    // 34. Find First and Last Position of Element in Sorted Array
+    // Given an array of integers nums sorted in non-decreasing order, find the starting and ending position
+    // of a given target value.
+    //If target is not found in the array, return [-1, -1]
+    //You must write an algorithm with O(log n) runtime complexity
+
+    protected int[] searchRange(int[] nums, int target) {
+        int[] result = new int[2];
+        result[0] = findFirstLast(nums, target, true);
+        result[1] = findFirstLast(nums, target, false);
+        return result;
+    }
+
+    private int findFirstLast(int[] nums, int target, boolean first) {
+        int idx = -1;
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < nums[mid] || (first && target == nums[mid])) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+            if (nums[mid] == target) {
+                idx = mid;
+            }
+        }
+        return idx;
     }
 
 }
--- test/binary/search/BinarySearchTest.java
+++ test/binary/search/BinarySearchTest.java
@@ -1,5 +1,6 @@
 package binary.search;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
@@ -14,7 +15,7 @@ class BinarySearchTest {
         // When
         int result = main.binarySearch(numbers, 1);
         // Then
-        assertEquals(result, 0);
+        assertEquals(0, result);
     }
 
     @Test
@@ -25,7 +26,59 @@ class BinarySearchTest {
         // When
         int result = main.binarySearch(numbers, 11);
         // Then
-        assertEquals(result, -1);
+        assertEquals(-1, result);
+    }
+
+    @Test
+    void searchRange() {
+        // Given
+        BinarySearch main = new BinarySearch();
+        int[] nums = {1, 3, 3, 5, 6, 8, 9};
+        int target = 3;
+        int[] expected = {1, 2};
+        // When
+        int[] result = main.searchRange(nums, target);
+        // Then
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    void searchRange1() {
+        // Given
+        BinarySearch main = new BinarySearch();
+        int[] nums = {5, 7, 7, 8, 8, 10};
+        int target = 8;
+        int[] expected = {3, 4};
+        // When
+        int[] result = main.searchRange(nums, target);
+        // Then
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    void searchRangeNotFound() {
+        // Given
+        BinarySearch main = new BinarySearch();
+        int[] nums = {2, 4, 7, 8, 10};
+        int target = 5;
+        int[] expected = {-1, -1};
+        // When
+        int[] result = main.searchRange(nums, target);
+        // Then
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    void searchRangeNotFound1() {
+        // Given
+        BinarySearch main = new BinarySearch();
+        int[] nums = {};
+        int target = 0;
+        int[] expected = {-1, -1};
+        // When
+        int[] result = main.searchRange(nums, target);
+        // Then
+        assertArrayEquals(expected, result);
     }
 
 }
