3a89c24b79cf12a802264f86ce2eeae09db867b9
Steampunk <dasies11@gmail.com>
2023-03-13 18:58:52 +0100

    Add rotting oranges to matrix problems

--- src/matrix/Main.java
+++ src/matrix/Main.java
@@ -1,9 +1,21 @@
 package matrix;
 
 import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
 
+    // store the position of rotten orange
+    static class Position {
+
+        int x;
+        int y;
+
+        Position(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
     // 200. Number of Islands
     // Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands
     // An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically
@@ -78,6 +90,121 @@ public class Main {
             dfsFill(grid, i - 1, j);
             dfsFill(grid, i, j + 1);
             dfsFill(grid, i, j - 1);
+        }
+    }
+
+    // 994. Rotting Oranges
+    // You are given an m x n grid where each cell can have one of three values:
+    // 0 representing an empty cell,
+    // 1 representing a fresh orange, or
+    // 2 representing a rotten orange.
+    // Every minute, any fresh orange that is 4-directionally adjacent to a rotten orange becomes rotten
+    //
+    // Return the minimum number of minutes that must elapse until no cell has a fresh orange. If this is impossible, return -1
+
+    // BFS:
+
+    protected int orangesRottingBFS(int[][] grid) {
+        Queue<Position> q = new LinkedList<>();
+        int total = 0, rotten = 0, time = 0;
+
+        // traverse the grid, offer position of rotten orange into queue, and count the total num of orange
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1 || grid[i][j] == 2) {
+                    total++;
+                }
+                if (grid[i][j] == 2) {
+                    q.offer(new Position(i, j));
+                }
+            }
+        }
+
+        // if there is no orange, return 0;
+        if (total == 0) {
+            return 0;
+        }
+
+        while (!q.isEmpty() && rotten < total) {
+            // size is the num of rotten oranges of the last round
+            int size = q.size();
+
+            // count the num of rotten oranges, if it equals to total num, return time;
+            rotten += size;
+            if (rotten == total) {
+                return time;
+            }
+
+            // every round, time ++
+            time++;
+
+            // Continue to dequeue until all rotten oranges of last round are removed from the queue
+            for (int i = 0; i < size; i++) {
+                Position p = q.peek();
+
+                // check the cell in the left/right/top/down of the rotten orange, if it is a fresh orange, enqueue it.
+                if (p.x + 1 < grid.length && grid[p.x + 1][p.y] == 1) {
+                    grid[p.x + 1][p.y] = 2;
+                    q.offer(new Position(p.x + 1, p.y));
+                }
+                if (p.x - 1 >= 0 && grid[p.x - 1][p.y] == 1) {
+                    grid[p.x - 1][p.y] = 2;
+                    q.offer(new Position(p.x - 1, p.y));
+                }
+                if (p.y + 1 < grid[0].length && grid[p.x][p.y + 1] == 1) {
+                    grid[p.x][p.y + 1] = 2;
+                    q.offer(new Position(p.x, p.y + 1));
+                }
+                if (p.y - 1 >= 0 && grid[p.x][p.y - 1] == 1) {
+                    grid[p.x][p.y - 1] = 2;
+                    q.offer(new Position(p.x, p.y - 1));
+                }
+                q.poll();
+            }
+        }
+        return -1;
+    }
+
+    // DFS:
+    protected int orangesRottingDFS(int[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return -1;
+        }
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 2) {
+                    rotAdjacent(grid, i, j, 2);
+                }
+            }
+        }
+
+        int minutes = 2;
+        for (int[] row : grid) {
+            for (int cell : row) {
+                if (cell == 1) {
+                    return -1;
+                }
+                minutes = Math.max(minutes, cell);
+            }
+        }
+
+        return minutes - 2;
+    }
+
+    private void rotAdjacent(int[][] grid, int i, int j, int minutes) {
+        if (i < 0 || i >= grid.length /* out of bounds */
+                || j < 0 || j >= grid[0].length /* out of bounds */
+                || grid[i][j] == 0 /* empty cell */
+                || (1 < grid[i][j] && grid[i][j] < minutes) /* this orange is already rotten by another rotten orange */
+        ) {
+            return;
+        } else {
+            grid[i][j] = minutes;
+            rotAdjacent(grid, i - 1, j, minutes + 1);
+            rotAdjacent(grid, i + 1, j, minutes + 1);
+            rotAdjacent(grid, i, j - 1, minutes + 1);
+            rotAdjacent(grid, i, j + 1, minutes + 1);
         }
     }
 
--- test/matrix/MainTest.java
+++ test/matrix/MainTest.java
@@ -1,6 +1,6 @@
 package matrix;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -16,8 +16,8 @@ class MainTest {
                 {'0', '0', '1', '0', '0'},
                 {'0', '0', '0', '1', '1'}
         };
-        // When
         int expected = 3;
+        // When
         int actual = main.numIslandsBFS(grid);
         // Then
         assertEquals(expected, actual);
@@ -33,11 +33,77 @@ class MainTest {
                 {'0', '0', '1', '0', '0'},
                 {'0', '0', '0', '1', '1'}
         };
-        // When
         int expected = 3;
+        // When
         int actual = main.numIslandsDFS(grid);
         // Then
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void orangesRottingBFS() {
+        // Given
+        Main main = new Main();
+        int[][] grid = {{2,1,1},{1,1,0},{0,1,1}};
+        int expected = 4;
+        // When
+        int result = main.orangesRottingBFS(grid);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void orangesRottingBFS1() {
+        // Given
+        Main main = new Main();
+        int[][] grid = {{2,1,1},{0,1,1},{1,0,1}};
+        int expected = -1;
+        // When
+        int result = main.orangesRottingBFS(grid);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void orangesRottingBFS2() {
+        // Given
+        Main main = new Main();
+        int[][] grid = {{0,2}};
+        int expected = 0;
+        // When
+        int result = main.orangesRottingBFS(grid);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void orangesRottingDFS() {
+        // Given
+        Main main = new Main();
+        int[][] grid = {{2,1,1},{1,1,0},{0,1,1}};
+        int expected = 4;
+        // When
+        int result = main.orangesRottingDFS(grid);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void orangesRottingDFS1() {
+        // Given
+        Main main = new Main();
+        int[][] grid = {{2,1,1},{0,1,1},{1,0,1}};
+        int expected = -1;
+        // When
+        int result = main.orangesRottingDFS(grid);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void orangesRottingDFS2() {
+        // Given
+        Main main = new Main();
+        int[][] grid = {{0,2}};
+        int expected = 0;
+        // When
+        int result = main.orangesRottingDFS(grid);
+        assertEquals(expected, result);
     }
 
 }
